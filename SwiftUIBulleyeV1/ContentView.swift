c28e17a1fee03fcdf6a17b51668d417fd39c28d8
Sungbin Ko <kosb7544@gmail.com>
2021-05-05 01:26:25 +0900

    Baseline

--- SwiftUIBulleyeV1/ContentView.swift
+++ SwiftUIBulleyeV1/ContentView.swift
@@ -2,16 +2,40 @@
 //  ContentView.swift
 //  SwiftUIBulleyeV1
 //
-//  Created by Release on 2021/05/04.
+//  Created by Releas	e on 2021/05/04.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+
+    @State var alertIsVisible: Bool = false
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+        VStack{
+            Text("Welcom to my first App")
+                .fontWeight(.semibold)
+                .foregroundColor(Color.green)
+            
+            Button(
+                action: {
+                print("Button pressed")
+                self.alertIsVisible = true
+                })
+            {
+                Text("Hit me")
+            }
+            
+            .alert(isPresented: self.$alertIsVisible) {
+                Alert(
+                    title: Text("Hello there!"),
+                    message: Text("This is my first pop-up"),
+                    dismissButton: .default(Text("Awesome"))
+                )
+            }
+            
+        } // End of Stack
+    } // End of body
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -19,3 +43,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
