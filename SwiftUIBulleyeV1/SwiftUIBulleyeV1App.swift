d13cbde6ac402ed9d626a425e42b1b81f9e84c4a
Sungbin Ko <kosb7544@gmail.com>
2021-05-04 21:08:13 +0900

    Initial Commit

---
+++ SwiftUIBulleyeV1/SwiftUIBulleyeV1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBulleyeV1App.swift
+//  SwiftUIBulleyeV1
+//
+//  Created by Release on 2021/05/04.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBulleyeV1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
