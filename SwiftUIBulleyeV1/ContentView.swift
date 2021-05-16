2701904626337a833af88eda217dbb40f433024c
Sungbin Ko <kosb7544@gmail.com>
2021-05-16 17:10:04 +0900

    Build basic user interface

--- SwiftUIBulleyeV1/ContentView.swift
+++ SwiftUIBulleyeV1/ContentView.swift
@@ -2,20 +2,37 @@
 //  ContentView.swift
 //  SwiftUIBulleyeV1
 //
-//  Created by Releas	e on 2021/05/04.
+//  Created by Releas    e on 2021/05/04.
 //
 
 import SwiftUI
 
 struct ContentView: View {
 
+    
+    // stage for user interface views
     @State var alertIsVisible: Bool = false
+    @State var sliderValue: Double = 50.0
 
     var body: some View {
         VStack{
-            Text("Welcom to my first App")
-                .fontWeight(.semibold)
-                .foregroundColor(Color.green)
+            Spacer()
+            HStack{
+                Text("Put the bull eye as close as you can do:")
+                Text("100")
+            }
+            
+            Spacer()
+            
+            // Slider row
+            // TODO: add view for the slide row here
+            HStack{
+                Text("1")
+                Slider(value: self.$sliderValue, in: 1...100)
+                Text("100")
+            }
+            
+            Spacer()
             
             Button(
                 action: {
@@ -29,15 +46,40 @@ struct ContentView: View {
             .alert(isPresented: self.$alertIsVisible) {
                 Alert(
                     title: Text("Hello there!"),
-                    message: Text("This is my first pop-up"),
+                    message: Text("The slider's value is \(Int(self.sliderValue.rounded()))."),
                     dismissButton: .default(Text("Awesome"))
                 )
             }
             
+            Spacer()
+            
+            // Score row
+            HStack{
+                Button(action:{}) {
+                    Text("Start over")
+                }
+
+                Spacer()
+                Text("Score:")
+                Text("999999")
+                
+                Spacer()
+                Text("Round:")
+                Text("999")
+
+                Spacer()
+                Button(action:{}) {
+                    Text("Inform")
+                }
+            }.padding(.bottom, 20)
+            
         } // End of Stack
     } // End of body
+    
+    
 }
 
+// preview
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
